1935e880643fc59f5df5f07af79a811576087ecc
vugar.mehraliyev33@gmail.com <vugar.mehraliyev33@gmail.com>
2021-05-14 22:59:58 +0400

    Created all services and implementing entity framework

---
+++ Core/DataAccess/EntityFramework/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+   public interface IEntityRepository<T>
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T GetById(Expression<Func<T, bool>> filter);
+    }
+}
---
+++ DataAccess/Concrete/Carcontext.cs
@@ -0,0 +1,20 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete
+{
+   public class Carcontext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-KESG5H22;database=RentCar;Trusted_Connection=true");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
